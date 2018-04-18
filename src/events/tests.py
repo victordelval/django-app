541f6b52047b8434faaab05158753b770e37cfef
victordelval <victor.val.andres@gmail.com>
2018-04-18 17:53:08 +0200

    Adds initial tests for views based on requests from a client

--- src/events/tests.py
+++ src/events/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class GetRequestsViewsTests(TestCase):
+    """
+    Set of tests for each view of the application, consisting of
+    launching a GET request from the client to check the status code
+    of the response.
+    """
+
+    def test_get_event_list_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_event_form_view(self):
+        response = self.client.get('/events')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_hashtag_form_view(self):
+        response = self.client.get('hashtag')
+        self.assertEqual(response.status_code, 200)
