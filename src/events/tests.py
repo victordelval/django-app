b18fcd68bcc804b893fa0d76ef7035c3711e4a48
victordelval <victor.val.andres@gmail.com>
2018-04-18 19:15:11 +0200

    Improves previous view's tests with rendered template check

--- src/events/tests.py
+++ src/events/tests.py
@@ -6,17 +6,20 @@ class GetRequestsViewsTests(TestCase):
     """
     Set of tests for each view of the application, consisting of
     launching a GET request from the client to check the status code
-    of the response.
+    of the response and the rendered template.
     """
 
     def test_get_event_list_view(self):
         response = self.client.get(reverse('events:event-list-view'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/event_list.html')
 
     def test_get_event_form_view(self):
         response = self.client.get(reverse('events:event-form-view'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/event_form.html')
 
-    def test_get_hashtag_form_view(self):
-        response = self.client.get(reverse('events:hashtag-form-view'))
+    def test_get_tag_form_view(self):
+        response = self.client.get(reverse('events:tag-form-view'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/tag_form.html')
